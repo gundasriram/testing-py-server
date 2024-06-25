0a370119d722535240a42e168a301daf19deb439
gundasriram <gundasriram.24@gmail.com>
2024-06-25 10:26:08 +0800

    Update getAll.py

--- services/getAll.py
+++ services/getAll.py
@@ -1,1 +1,18 @@
+import boto3
+dynamodb = boto3.resource(
+    'dynamodb',
+    region_name="ap-southeast-1"
+)
+TABLE_CALL_ANALYSIS='call-analysis-table'
 
+def getAllCalls:
+  analysisTable = dynamodb.Table(TABLE_CALL_ANALYSIS)
+  response = table.query(
+      KeyConditionExpression=boto3.dynamodb.conditions.Key('PrimaryKey').eq('call')
+  )
+  # Get the items from the response
+  items = response.get('Items', [])
+  # Print the items
+  for item in items:
+      print(item)
+  return items
