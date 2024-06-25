7badc6b42c60e57cc737ffccaadd1cbf30311d4e
gundasriram <gundasriram.24@gmail.com>
2024-06-25 09:59:17 +0800

    Update server.py

--- server.py
+++ server.py
@@ -5,16 +5,13 @@ from dotenv import load_dotenv
 
 @app.route('/api/analysis', methods=['POST'])
 def handler():
-    try:
-        data = request.json
-        finalresponse = callAnalysis(data)
-        # Process the data as needed
-        response = {
-            'message': 'First API received the data!',
-            'received_data': finalresponse
-        }
-        return jsonify(response), 200
-    expect ClientError as e:
-        return jsonify({'message' : 'Internal Server Error'}), 500
+    data = request.json
+    finalresponse = callAnalysis(data)
+    # Process the data as needed
+    response = {
+        'message': 'First API received the data!',
+        'received_data': finalresponse
+    }
+    return jsonify(response), 200
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
