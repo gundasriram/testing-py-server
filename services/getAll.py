ab88531eb4b6eb53a8fdef30ac296e4954672cc9
gundasriram <gundasriram.24@gmail.com>
2024-06-25 10:35:12 +0800

    Update getAll.py

--- services/getAll.py
+++ services/getAll.py
@@ -9,7 +9,7 @@ def getAllCalls(data):
   try:
     analysisTable = dynamodb.Table(TABLE_CALL_ANALYSIS)
     response = analysisTable.query(
-      KeyConditionExpression=boto3.dynamodb.conditions.Key('type').eq('call')
+      KeyConditionExpression=boto3.dynamodb.conditions.Key('type').eq('CALL')
     )
     # Get the items from the response
     items = response.get('Items', [])
