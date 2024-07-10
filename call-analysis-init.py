677ed06daa762b9fa07ce5184a8f0700f7c59a07
gundasriram <gundasriram.24@gmail.com>
2024-07-10 15:50:30 +0800

    Update call-analysis-init.py

--- call-analysis-init.py
+++ call-analysis-init.py
@@ -24,8 +24,8 @@ def init():
             call_id = call['call_id']
             print('call_id', call_id)
             updated_call_id = call_id.replace("PENDING", "INPROGRESS")
-            params = {
-                 Key={
+            response = analysisTable.update_item(
+                Key={
                     'type': 'CALL',
                     # 'call_id': call['call_id']
                 },
@@ -35,10 +35,6 @@ def init():
                 },
                 ConditionExpression="attribute_exists(type)",
                 ReturnValues="UPDATED_NEW"
-            }
-            print('params', params)
-            response = analysisTable.update_item(
-               params
             )
             callAnalysis(body, call_id)
     except Exception as e:
