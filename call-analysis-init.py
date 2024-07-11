cfdbffe116c1f77b3c5d41ae41518d0333415a0f
gundasriram <gundasriram.24@gmail.com>
2024-07-11 09:55:49 +0800

    Update call-analysis-init.py

--- call-analysis-init.py
+++ call-analysis-init.py
@@ -7,25 +7,34 @@ dynamodb = boto3.resource(
 from services.analysis import callAnalysis
 
 TABLE_CALL_ANALYSIS='call-analysis-table'
+analysisTable = dynamodb.Table(TABLE_CALL_ANALYSIS)
+
+def checkTaskStatus(call_id):
+    response = table.query(
+        KeyConditionExpression=boto3.dynamodb.conditions.Key('type').eq('CALL') &
+                         boto3.dynamodb.conditions.Key('call_id').eq(call_id)
+    )
+    return response
 
 def init():
     try:
-        analysisTable = dynamodb.Table(TABLE_CALL_ANALYSIS)
         pending_call_analysis = analysisTable.query(
-        KeyConditionExpression=Key('type').eq('CALL') & Key('call_id').begins_with('PENDING')
+            KeyConditionExpression=Key('type').eq('CALL') & Key('call_id').begins_with('PENDING')
         )
         print('pending_call_analysis', pending_call_analysis)
         for call in pending_call_analysis['Items']:
             file = call['s3_file_path']
             call_id = call['call_id']
             print('call_id', call_id)
+            taskStatus = checkTaskStatus(call_id)
+            print('taskStatus', taskStatus)
             updated_call_id = call_id.replace("PENDING", "INPROGRESS")
             print('updated_call_id', updated_call_id)
             params = {'type': 'CALL', 'call_id': call_id}
             print('params', params)
             analysisTable.delete_item(Key=params)
             call['call_id'] = updated_call_id
-            response = analysisTable.put_item(Item=item)
+            response = analysisTable.put_item(Item=call)
             callAnalysis(file, call)
     except Exception as e:
         print('Error in callAnalysis init :::', e)
