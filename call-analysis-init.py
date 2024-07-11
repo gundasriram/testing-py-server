661948cea709123cb154912d3a846d00e3ebd86c
gundasriram <gundasriram.24@gmail.com>
2024-07-11 11:04:58 +0800

    Update call-analysis-init.py

--- call-analysis-init.py
+++ call-analysis-init.py
@@ -14,28 +14,35 @@ def checkTaskStatus(call_id):
         KeyConditionExpression=boto3.dynamodb.conditions.Key('type').eq('CALL') &
                          boto3.dynamodb.conditions.Key('call_id').eq(call_id)
     )
-    return response
+    if response and len(response.Items) > 0:
+        return True
+    else:
+        return False
 
 def init():
+    print('*************** CALL ANALYSIS INIT ***************')
     try:
         pending_call_analysis = analysisTable.query(
             KeyConditionExpression=Key('type').eq('CALL') & Key('call_id').begins_with('PENDING')
         )
-        print('pending_call_analysis', pending_call_analysis)
+        print('*************** Pending Calls START ***************')
+        print(pending_call_analysis)
+        print('*************** Pending Calls END***************')
         for call in pending_call_analysis['Items']:
             file = call['s3_file_path']
             call_id = call['call_id']
-            print('call_id', call_id)
+            print('*************** Started Process for Call_id :::', call_id)
             taskStatus = checkTaskStatus(call_id)
-            print('taskStatus', taskStatus)
-            updated_call_id = call_id.replace("PENDING", "INPROGRESS")
-            print('updated_call_id', updated_call_id)
-            params = {'type': 'CALL', 'call_id': call_id}
-            print('params', params)
-            analysisTable.delete_item(Key=params)
-            call['call_id'] = updated_call_id
-            response = analysisTable.put_item(Item=call)
-            callAnalysis(file, call)
+            print('*************** taskStatus :::', taskStatus)
+            if(taskStatus):
+                updated_call_id = call_id.replace("PENDING", "INPROGRESS")
+                print('*************** updated_call_id :::', updated_call_id)
+                params = {'type': 'CALL', 'call_id': call_id}
+                analysisTable.delete_item(Key=params)
+                call['call_id'] = updated_call_id
+                response = analysisTable.put_item(Item=call)
+                print('*************** Making Call to CallAnalysis with File and Call data ***************')
+                callAnalysis(file, call)
     except Exception as e:
         print('Error in callAnalysis init :::', e)
         raise Exception(f"Error in callAnalysis init: {e}")
