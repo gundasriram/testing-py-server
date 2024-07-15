a123c7e9bc79a1f822cafbbd2a4466ddeb4ff6ea
gundasriram <gundasriram.24@gmail.com>
2024-07-15 10:11:36 +0800

    Update call-analysis-init.py

--- call-analysis-init.py
+++ call-analysis-init.py
@@ -1,48 +1,34 @@
-import boto3
-from boto3.dynamodb.conditions import Key
-dynamodb = boto3.resource(
-    'dynamodb',
-    region_name="ap-southeast-1"
-  )
-from services.analysis import callAnalysis
 
-TABLE_CALL_ANALYSIS='call-analysis-table'
-analysisTable = dynamodb.Table(TABLE_CALL_ANALYSIS)
+import boto3
+from services.analysis import callAnalysis
+from services.db.db_connection import db
 
 def checkTaskStatus(call_id):
-    response = analysisTable.query(
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('type').eq('CALL') &
-                         boto3.dynamodb.conditions.Key('call_id').eq(call_id)
-    )
-    if response and len(response['Items']) > 0:
-        return True
-    else:
+    res = db.getOneAnalysis(call_id)
+    if data['task_status'] == 'INPROGRESS':
         return False
+    else:
+        return True
 
 def init():
     print('*************** CALL ANALYSIS INIT ***************')
     try:
-        pending_call_analysis = analysisTable.query(
-            KeyConditionExpression=Key('type').eq('CALL') & Key('call_id').begins_with('PENDING')
-        )
+        db.connect()
+        pending_call_analysis = db.getAllPendingTask()
         print('*************** Pending Calls START ***************')
         print(pending_call_analysis)
         print('*************** Pending Calls END***************')
-        for call in pending_call_analysis['Items']:
+        for call in pending_call_analysis:
             file = call['s3_file_path']
             call_id = call['call_id']
             print('*************** Started Process for Call_id :::', call_id)
             taskStatus = checkTaskStatus(call_id)
             print('*************** taskStatus :::', taskStatus)
             if(taskStatus):
-                updated_call_id = call_id.replace("PENDING", "INPROGRESS")
-                print('*************** updated_call_id :::', updated_call_id)
-                params = {'type': 'CALL', 'call_id': call_id}
-                analysisTable.delete_item(Key=params)
-                call['call_id'] = updated_call_id
-                response = analysisTable.put_item(Item=call)
+                db.updateTaskStatusforCallId('INPROGRESS', call_id)
                 print('*************** Making Call to CallAnalysis with File and Call data ***************')
                 callAnalysis(file, call)
+        db.close()
     except Exception as e:
         print('Error in callAnalysis init :::', e)
         raise Exception(f"Error in callAnalysis init: {e}")
