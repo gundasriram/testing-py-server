afbf3c6a2aa79c6a56f85a7da0f6873c0566f4eb
gundasriram <gundasriram.24@gmail.com>
2024-07-15 12:51:24 +0800

    Update db_connection.py

--- services/db/db_connection.py
+++ services/db/db_connection.py
@@ -75,8 +75,9 @@ class Database:
             where_clause_str = ' AND '.join([f"{key} = %s" for key in where_clause.keys()])
             query = f"UPDATE call_analysis SET {set_clause} WHERE {where_clause_str}"
             values = list(analysis_data.values()) + list(where_clause.values())
-            print('query', query),
-            print('values', values )
+            print('*************** query', query),
+            print('*************** values ', values)
+            print('*************** analysis_data ', analysis_data)
             cursor = self.conn.cursor(dictionary=True)
             cursor.execute(query, tuple(values))
             mydb.commit()
