493744cc12c3e07065a03a9600711b025e70fe08
gundasriram <gundasriram.24@gmail.com>
2024-07-16 11:30:58 +0800

    Update db_connection.py

--- services/db/db_connection.py
+++ services/db/db_connection.py
@@ -2,11 +2,11 @@ import os
 import mysql.connector
 import json
 
-MYSQL_HOST= os.environ['MYSQL_HOST']
-MYSQL_USER= os.environ['MYSQL_USER']
-MYSQL_PASSWORD= os.environ['MYSQL_PASSWORD']
-MYSQL_PORT= os.environ['MYSQL_PORT']
-MYSQL_DATABASE= os.environ['MYSQL_DATABASE']
+# MYSQL_HOST= os.environ['MYSQL_HOST']
+# MYSQL_USER= os.environ['MYSQL_USER']
+# MYSQL_PASSWORD= os.environ['MYSQL_PASSWORD']
+# MYSQL_PORT= os.environ['MYSQL_PORT']
+# MYSQL_DATABASE= os.environ['MYSQL_DATABASE']
 
 class Database:
     def __init__(self):
@@ -17,11 +17,11 @@ class Database:
             if self.conn is None:
                 self.conn = mysql.connector.connect(
                     auth_plugin='mysql_native_password',
-                    host=MYSQL_HOST,
-                    user=MYSQL_USER,
-                    password=MYSQL_PASSWORD,
-                    port=MYSQL_PORT,
-                    database=MYSQL_DATABASE,
+                    host="localhost",
+                    user="root",
+                    password="admin",
+                    port=3306,
+                    database="world",
                 )
             return self.conn
         except Exception as e:
@@ -33,49 +33,64 @@ class Database:
             self.conn.close()
             self.conn = None
 
-    def getAllPendingTask(self):
-        try:
-            print('*************** getAllPending ***************')
-            query = 'SELECT * from call_analysis where task_status = "PENDING" LIMIT 100'
-            cursor = self.conn.cursor(dictionary=True)
-            cursor.execute(query)
-            rows = cursor.fetchall()
-            json_data = json.dumps(rows, indent=4) 
-            cursor.close()
-            return json.loads(json_data)
-        except Exception as e:
-            print('Error in getAllPending init :::', e)
-            raise Exception(f"Error in getAllPending init: {e}")
+def get_db_connection():
+    try:
+        print('*************** CONNECTING TO DB ***************')
+        return mysql.connector.connect(
+             auth_plugin='mysql_native_password',
+            host="localhost",
+            user="root",
+            password="admin",
+            port=3306,
+            database="world",
+        )
+    except Exception as e:
+        print('Error in connect DB init :::', e)
+        raise Exception(f"Error in connect init: {e}")
 
-    def updateTaskStatusforCallId(self, status, call_id):
-        try:
-            print('*************** updateTaskStatusforCallId ***************')
-            query = "UPDATE call_analysis SET task_status = %s where call_id = %s"
-            print('query', query)
-            cursor = self.conn.cursor(dictionary=True)
-            cursor.execute(query, (status, call_id))
-            self.conn.commit()
-            cursor.close()
-        except Exception as e:
-            print('Error in updateTaskStatusforCallId :::', e)
-            raise Exception(f"Error in updateTaskStatusforCallId: {e}")
+def getAllPendingTask(self):
+    try:
+        print('*************** getAllPending ***************')
+        query = 'SELECT * from call_analysis where task_status = "PENDING" LIMIT 100'
+        cursor = self.cursor(dictionary=True)
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        json_data = json.dumps(rows, indent=4) 
+        cursor.close()
+        return json.loads(json_data)
+    except Exception as e:
+        print('Error in getAllPending init :::', e)
+        raise Exception(f"Error in getAllPending init: {e}")
 
-    def getOneAnalysis(self, call_id):
-        try:
-            print('*************** getOneAnalysis ***************')
-            query = "SELECT * from call_analysis where call_id = %s"
-            print('query', query)
-            cursor = self.conn.cursor(dictionary=True)
-            cursor.execute(query, (call_id,))
-            rows = cursor.fetchone()
-            json_data = json.dumps(rows, indent=4) 
-            cursor.close()
-            return json.loads(json_data)
-        except Exception as e:
-            print('Error in getOneAnalysis :::', e)
-            raise Exception(f"Error in getOneAnalysis: {e}")
+def updateTaskStatusforCallId(self, status, call_id):
+    try:
+        print('*************** updateTaskStatusforCallId ***************')
+        query = "UPDATE call_analysis SET task_status = %s where call_id = %s"
+        print('query', query)
+        cursor = self.cursor(dictionary=True)
+        cursor.execute(query, (status, call_id))
+        self.commit()
+        cursor.close()
+    except Exception as e:
+        print('Error in updateTaskStatusforCallId :::', e)
+        raise Exception(f"Error in updateTaskStatusforCallId: {e}")
 
-    def updateFinalAnalysis(self, where_clause, analysis_data, transcription_whisper):
+def getOneAnalysis(self, call_id):
+    try:
+        print('*************** getOneAnalysis ***************')
+        query = "SELECT * from call_analysis where call_id = %s"
+        print('query', query)
+        cursor = self.cursor(dictionary=True)
+        cursor.execute(query, (call_id,))
+        rows = cursor.fetchone()
+        json_data = json.dumps(rows, indent=4) 
+        cursor.close()
+        return json.loads(json_data)
+    except Exception as e:
+        print('Error in getOneAnalysis :::', e)
+        raise Exception(f"Error in getOneAnalysis: {e}")
+
+def updateFinalAnalysis(self, where_clause, analysis_data, transcription_whisper):
         try:
             print('*************** updateFinalAnalysis Start ***************')
             analysis_data['transcription_whisper'] = transcription_whisper
@@ -102,9 +117,9 @@ class Database:
             print('*************** query', query)
             print('*************** analysis_data ', analysis_data)
             print('*************** values', values)
-            cursor = self.conn.cursor()
+            cursor = self.cursor()
             cursor.execute(query, values)
-            self.conn.commit()
+            self.commit()
             cursor.close()
             print('*************** updateFinalAnalysis END ***************')
         except Exception as e:
@@ -112,4 +127,4 @@ class Database:
             raise Exception(f"Error in updateFinalAnalysis: {e}")
 
 # Singleton instance to be used throughout the application
-db = Database()
+# db = Database()
