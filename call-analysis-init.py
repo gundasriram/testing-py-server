1ab64dd594de1093bb2de6d95f9949434f011983
gundasriram <gundasriram.24@gmail.com>
2024-07-16 17:11:36 +0800

    Update call-analysis-init.py

--- call-analysis-init.py
+++ call-analysis-init.py
@@ -2,7 +2,6 @@
 import boto3
 from services.db.db_connection import get_db_connection, updateTaskStatusforCallId, getOneAnalysis, getAllPendingTask
 from services.analysis import callAnalysis
-import concurrent.futures
 
 def checkTaskStatus(call_id, db):
     res = getOneAnalysis(db, call_id)
@@ -19,32 +18,19 @@ def init():
         print('*************** Pending Calls START ***************')
         print(pending_call_analysis)
         print('*************** Pending Calls END***************')
-        # Function to process a single call
-        def process_call(call):
-            db = get_db_connection()
+        for call in pending_call_analysis:
             print('call', call)
             file = call['s3_file_path']
             call_id = call['call_id']
             print('*************** Started Process for Call_id :::', call_id)
-            taskStatus = checkTaskStatus(call_id, db)
+            taskStatus = checkTaskStatus(call_id)
             print('*************** taskStatus :::', taskStatus)
-            if taskStatus:
+            if(taskStatus):
                 updateTaskStatusforCallId(db, 'INPROGRESS', call_id)
                 print('*************** Making Call to CallAnalysis with File and Call data ***************')
                 callAnalysis(file, call, db)
-            db.close()
-        # Use ThreadPoolExecutor to run calls in parallel
-        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            # Group pending calls in pairs of two
-            for i in range(0, len(pending_call_analysis), 2):
-                calls_to_process = pending_call_analysis[i:i+2]
-                # Submit tasks to the executor
-                futures = [executor.submit(process_call, call) for call in calls_to_process]
-                # Wait for the futures to complete
-                concurrent.futures.wait(futures)
         db.close()
     except Exception as e:
-        db.close()
         print('Error in callAnalysis init :::', e)
         raise Exception(f"Error in callAnalysis init: {e}")
 
